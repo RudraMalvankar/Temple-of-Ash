369291cd4409ad9562465e99f57fdd9f2eff6c78
RudraMalvankar <rudracmalvankar@gmail.com>
2026-07-15 20:30:27 +0530

    fix: Redesign level definitions to guarantee solvability and prevent layout deadlocks

--- src/client/levels/levelDefinitions.ts
+++ src/client/levels/levelDefinitions.ts
@@ -25,51 +25,39 @@ export const LEVELS: LevelDefinition[] = [
   {
     id: 1,
     name: "1. Dungeon Entry",
-    gridWidth: 10,
-    gridHeight: 9,
+    gridWidth: 9,
+    gridHeight: 5,
     layout: [
-      "W W W W W W W W W W",
-      "W P . . W . . . . W",
-      "W . T . W . . T . W",
-      "W . . . W . . . . W",
-      "W . . . . . . . . W",
-      "W . . . W . . . . W",
-      "W . T . W . . T . W",
-      "W . . . W . . X . W",
-      "W W W W W W W W W W"
+      "W W W W W W W W W",
+      "W P . . W . . X W",
+      "W W W . W . W W W",
+      "W . . . . . . . W",
+      "W W W W W W W W W"
     ]
   },
   {
     id: 2,
     name: "2. Crate Pushing",
-    gridWidth: 10,
-    gridHeight: 9,
+    gridWidth: 9,
+    gridHeight: 5,
     layout: [
-      "W W W W W W W W W W",
-      "W P . . W . . . . W",
-      "W . . . W . . . . W",
-      "W . . C . . . . . W",
-      "W W W W W . W W W W",
-      "W . . . W . W . . W",
-      "W . T . . . . . . W",
-      "W . . . W . W . X W",
-      "W W W W W W W W W W"
+      "W W W W W W W W W",
+      "W P . . C . . X W",
+      "W W W W . W W W W",
+      "W . . . . . . . W",
+      "W W W W W W W W W"
     ]
   },
   {
     id: 3,
     name: "3. Pressure Plates",
     gridWidth: 10,
-    gridHeight: 9,
+    gridHeight: 5,
     layout: [
       "W W W W W W W W W W",
-      "W P . . W . . . . W",
-      "W . . . W . . . . W",
-      "W . . C . . O . . W",
-      "W W W W W . W W W W",
-      "W . . . W . W . . W",
-      "W . T . . . . . . W",
-      "W . . . W . W . X W",
+      "W P . . C . . O . W",
+      "W W W W . W W W W W",
+      "W . . . . . . . X W",
       "W W W W W W W W W W"
     ]
   },
@@ -77,16 +65,13 @@ export const LEVELS: LevelDefinition[] = [
     id: 4,
     name: "4. Doors & Gates",
     gridWidth: 11,
-    gridHeight: 9,
+    gridHeight: 6,
     layout: [
       "W W W W W W W W W W W",
-      "W P . . W . . . . . W",
-      "W . . . W . . . . . W",
-      "W . . C . . O . . . W",
-      "W W W W W . W W D W W",
-      "W . . . W . W . . . W",
-      "W . T . . . . . . . W",
-      "W . . . W . W . . X W",
+      "W P . . C . . O . . W",
+      "W W W W . W W W W D W",
+      "W . . . . . . . . . W",
+      "W . . . . . . . . X W",
       "W W W W W W W W W W W"
     ]
   },
@@ -94,16 +79,14 @@ export const LEVELS: LevelDefinition[] = [
     id: 5,
     name: "5. Lava Pools",
     gridWidth: 12,
-    gridHeight: 9,
+    gridHeight: 7,
     layout: [
       "W W W W W W W W W W W W",
       "W P . . . W . . . . . W",
-      "W . . C . W . . O . . W",
-      "W W W W L L L L W W W W",
-      "W . . W L L L L W . . W",
-      "W . . W L L L L W . . W",
-      "W . . W L L L L W . . W",
-      "W . . . . . X . . . . W",
+      "W . . C . . . . . O . W",
+      "W W W . L L L L . W W W",
+      "W . . . L L L L . . . W",
+      "W . . . . X . . . . . W",
       "W W W W W W W W W W W W"
     ]
   },
@@ -111,15 +94,14 @@ export const LEVELS: LevelDefinition[] = [
     id: 6,
     name: "6. Laser Beams",
     gridWidth: 12,
-    gridHeight: 9,
+    gridHeight: 7,
     layout: [
       "W W W W W W W W W W W W",
-      "W P . . W . . . . . . W",
-      "W . . C W . . . . . . W",
-      "W . W . W . . . . . . W",
-      "W . W . W V . . . . . W",
-      "W . W . W . . . . X . W",
-      "W . W . W . . . . . . W",
+      "W P . . . . . . . . . W",
+      "W . . C . . . . . . . W",
+      "W W W W W V W W W W W W",
+      "W . . . . . . . . . . W",
+      "W . . . . X . . . . . W",
       "W W W W W W W W W W W W"
     ]
   },
@@ -127,15 +109,13 @@ export const LEVELS: LevelDefinition[] = [
     id: 7,
     name: "7. Bridge Puzzles",
     gridWidth: 12,
-    gridHeight: 9,
+    gridHeight: 7,
     layout: [
       "W W W W W W W W W W W W",
-      "W P . . . . W . . . . W",
-      "W . . C . . W . . O . W",
-      "W W W W L L L L W W W W",
-      "W . . W L L L L W . . W",
-      "W . . B . . . . B . . W",
-      "W . . W L L L L W . . W",
+      "W P . . . . . . . . . W",
+      "W . . C . . . . . O . W",
+      "W W W W W B W W W W W W",
+      "W . . . . . . . . . . W",
       "W . . . . X . . . . . W",
       "W W W W W W W W W W W W"
     ]
@@ -144,16 +124,14 @@ export const LEVELS: LevelDefinition[] = [
     id: 8,
     name: "8. Temple of Ash",
     gridWidth: 14,
-    gridHeight: 10,
+    gridHeight: 8,
     layout: [
       "W W W W W W W W W W W W W W",
-      "W . . . . . . T . . . . . W",
-      "W . P . . . . . . . . K . W",
-      "W . . . . C . . . C . . . W",
+      "W P . . . O . . . . . K . W",
+      "W . . C . . . . . . C . . W",
       "W W W W W W D D W W W W W W",
       "W . . . . . . . . . . . . W",
-      "W . O . . . . . . . . O . W",
-      "W . . . . . Y . . . . . . W",
+      "W . . . . . Y . . . . O . W",
       "W . . . . . X . . . . . . W",
       "W W W W W W W W W W W W W W"
     ]
